6fb6722b07986cc85e0817441edea479d59d6ba1
Jimena-QM <jimenaqm@gmail.com>
2022-06-12 18:40:32 -0500

    Completed app.js to filter table based on date

--- js/app.js
+++ js/app.js
@@ -21,4 +21,26 @@ data.forEach((dataRow) => {
     }
     );
 });
-}
+};
+
+// Function to filter table data using D3
+function handleClick() {
+    // Garb the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    // Check to see if a date was entered and filter the data using that date
+    if (date) {
+        // Apply 'filter' to the table data to only keep the
+        // rows where the 'datetime' value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    // Rebuild the table using the filtered data
+    // if no date is entered, then filteredData will be the original tableData
+    buildTable(filteredData);
+};
+// Attach event to listen for the form button with a click
+d3.selectAll("#filter-btn").on("click", handleClick);
+// Build the table when the page loads
+buildTable(tableData);
